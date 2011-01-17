e2bd38b43659235c8a00e21608385f8b0cf697f8
Julioj <jjmblanco23@hotmail.com>
2011-01-17 15:02:21 -0430

    segundo commit: escenario II

--- features/steps_definitions/articles_steps.rb
+++ features/steps_definitions/articles_steps.rb
@@ -1,8 +1,6 @@
 Given /^an article with the title "([^"]*)" And an article with the title "([^"]*)"$/ do |arg1, arg2|
-  @article1 = Article.create
-  @article2 = Article.create
-  @article1.title = arg1
-  @article2.title = arg2
+  @article1 = Article.create(:title=>arg1)
+  @article2 = Article.create(:title=>arg2)
 end
 
 When /^I am on the articles page$/ do
@@ -14,14 +12,19 @@ Then /^I should see "([^"]*)" And I should see "([^"]*)"$/ do |arg1, arg2|
   @article2.title.should == arg2
 end
 
-Given /^an article with the title "([^"]*)" and the content "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Given /^an article with the title "([^"]*)" And the content "([^"]*)"$/ do |arg1, arg2|
+  @article1 = Article.create(:title=>arg1,:body=>arg2)
 end
 
 When /^I am on the articles page And I follow "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  visit articles_path
+  click_link arg1
 end
 
 Then /^I should see "([^"]*)" And I should see "([^"]*)" And I should see "([^"]*)"$/ do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
+  visit articles_path
+  click_link arg1
+  response.should contain(arg1)
+  response.should contain(arg2)
+  response.should contain(arg3)
 end
