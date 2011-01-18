de68f1c810701f03509af446da9ce2a52e91299d
Julioj <jjmblanco23@hotmail.com>
2011-01-18 09:52:50 -0430

    Commit 3: correccion de features y steps

--- features/browse_articles.feature
+++ features/browse_articles.feature
@@ -4,7 +4,16 @@ Feature: Browse articles
  I want to be able to choose an article from a full list of articles and read it
 
  Scenario: Browsing a list of articles
-   Given an article with the title "La respuesta a todas las preguntas" And an article with the title "Nuevo lorem ipsum"
+   Given an article with the title "La respuesta a todas las preguntas" 
+   And an article with the title "Nuevo lorem ipsum"
    When I am on the articles page
-   Then I should see "La respuesta a todas las preguntas" And I should see "Nuevo lorem ipsum"
+   Then I should see "La respuesta a todas las preguntas" 
+   And I should see "Nuevo lorem ipsum"
 
+ Scenario: Reading an article
+   Given an article with the title "La respuesta a todas las preguntas" and the content "no hay tal cosa"
+   When I am on the articles page 
+   And I follow "La respuesta a todas las preguntas"
+   Then I should see "La respuesta a todas las preguntas" 
+   And I should see "no hay tal cosa" 
+   And I should see "Volver a artículos"
--- features/steps_definitions/articles_steps.rb
+++ features/steps_definitions/articles_steps.rb
@@ -1,30 +1,20 @@
-Given /^an article with the title "([^"]*)" And an article with the title "([^"]*)"$/ do |arg1, arg2|
+Given /^an article with the title "([^"]*)"$/ do |arg1|
   @article1 = Article.create(:title=>arg1)
-  @article2 = Article.create(:title=>arg2)
 end
 
 When /^I am on the articles page$/ do
   visit articles_path
 end
 
-Then /^I should see "([^"]*)" And I should see "([^"]*)"$/ do |arg1, arg2|
-  @article1.title.should == arg1
-  @article2.title.should == arg2
+Then /^I should see "([^"]*)"$/ do |arg1|
+  response.should contain(arg1)
 end
 
-Given /^an article with the title "([^"]*)" And the content "([^"]*)"$/ do |arg1, arg2|
+Given /^an article with the title "([^"]*)" and the content "([^"]*)"$/ do |arg1, arg2|
   @article1 = Article.create(:title=>arg1,:body=>arg2)
 end
 
-When /^I am on the articles page And I follow "([^"]*)"$/ do |arg1|
-  visit articles_path
-  click_link arg1
+When /^I follow "([^"]*)"$/ do |arg1|
+  click_link(arg1)
 end
 
-Then /^I should see "([^"]*)" And I should see "([^"]*)" And I should see "([^"]*)"$/ do |arg1, arg2, arg3|
-  visit articles_path
-  click_link arg1
-  response.should contain(arg1)
-  response.should contain(arg2)
-  response.should contain(arg3)
-end
